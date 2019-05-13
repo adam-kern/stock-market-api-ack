cfd30a668046fdf3e2c4b1813bf4fb609e5af9a1
adam-kern <adam31kern@gmail.com>
2019-05-13 18:40:12 -0400

    first commit

---
+++ stocks.js
@@ -0,0 +1,35 @@
+const baseURL = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=';
+let key = '85OBLISYS5QEV0J0';
+let url;
+
+let symbol = document.querySelector('.search');
+let searchForm = document.querySelector('form');
+let submitBtn = document.querySelector('.submit');
+let stockData = document.getElementById('stockData');
+
+searchForm.addEventListener('submit', submitSearch);
+function submitSearch(e) {
+  fetchResults(e);
+}
+function fetchResults(e) {
+  e.preventDefault();
+  url = baseURL + symbol.value + '&apikey=' + key;
+  console.log(url);
+  fetch(url).then(function (result) {
+    return result.json();
+  }).then(function (json) {
+    console.log(json);
+    displayResults(json);
+  });
+  }
+  function displayResults(json) {
+    let keys = Object.keys(json['Global Quote']);
+    for(let i = 1; i < 5; i++) { 
+      console.log(keys[i]);
+      let p = document.createElement('p');
+      let stocks = json['Global Quote'][keys[i]]; 
+      stockData.appendChild(p);
+      p.innerHTML = stocks + '<br>';
+
+    }
+  }
